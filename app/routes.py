66ac5f17fdc229eb8f33e28405475d6a0acc7fb6
dsemen <x-fly@list.ru>
2021-10-18 23:37:00 +0300

    initial commit

--- app/routes.py
+++ app/routes.py
@@ -3,4 +3,13 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'Hello, world!'
+    user = {'username': 'Semen'}
+    return '''
+<html>
+    <head>
+        <title>Home page - Microblog</title>
+    </head>
+    <body>
+        <h1>Hello, ''' + user['username'] +  '''!</h1>
+    </body>
+</html>'''
